f3e344d7ceec556952c9c9caeee93d6fd2e2362b
Jyoti152317 <engjyoti18@gmail.com>
2025-01-01 14:10:27 +0530

    index.html, style.css and navbar.js is added

---
+++ scripts/navbar.js
@@ -0,0 +1,25 @@
+const navbar = () => {
+  let nav = ` 
+  <nav class="navbar">
+      <div class="logo">
+        <a href="index.html">crom<span class="macron">a</span></a>
+      </div>
+      <div class="toggle-menu" id="toggle-menu">
+        <i class="fa fa-bars"></i>Menu
+      </div>
+      <div class="search-bar">
+        <input type="text" placeholder="What are you looking for" />
+        <button type="button" class="search-btn">
+          <i class="fas fa-search"></i>
+        </button>
+      </div>
+      <div class="nav-icons">
+        <a href="#" class="icon"><i class="fas fa-map-marker-alt"></i></a>
+        <a href="#" class="icon user"><i class="fas fa-user"></i></a>
+        <a href="#" class="icon"><i class="fas fa-shopping-cart"></i></a>
+      </div>
+    </nav>
+    `;
+  document.getElementById("nav").innerHTML = nav;
+};
+navbar();
